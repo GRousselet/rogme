7a9da8c87eb77d6c0b82ae10efed9d5d0ba3b4f0
Guillaume Rousselet <Guillaume.Rousselet@glasgow.ac.uk>
2017-02-04 13:10:28 +0000

    update & document 4 sf functions

--- R/shift_function_calc.R
+++ R/shift_function_calc.R
@@ -24,9 +24,10 @@
 #' quantile estimator. Biometrika, 69, 635-640. Wilcox, R.R. (1995) Comparing
 #' Two Independent Groups Via Multiple Quantiles. Journal of the Royal
 #' Statistical Society. Series D (The Statistician), 44, 91-99.
-#' @param df A data frame in tidy format. Column 1 describes the two groups;
+#' @param data A data frame in tidy format. Column 1 describes the two groups;
 #'   column 2 contains the values for each group. A properly formatted data
-#'   frame can be created using \code{link{mkt2}}.
+#'   frame can be created using \code{link{mkt2}}. Missing values are not
+#'   allowed.
 #' @param formula A formula with format response variable ∼ predictor variable,
 #'   where ~ (tilde) means "is modeled as a function of".
 #' @param nboot The number of bootstrap samples - default = 200.
@@ -47,7 +48,7 @@
 shifthd <- function(data = df, formula = obs ~ gr, nboot = 200){
   # subset data
   out <- subset_data2(data, formula)
-  df <- na.omit(df) # remove NA
+  #df <- na.omit(df) # remove NA
   x <- out$x
   y <- out$y
   gr_name1 <- out$gr_name1
@@ -107,9 +108,10 @@ shifthd <- function(data = df, formula = obs ~ gr, nboot = 200){
 #'   Wilcox, R.R. (2012) Introduction to robust estimation and hypothesis
 #'   testing. Academic Press, San Diego, CA.
 #'
-#' @param df A data frame in tidy format. Column 1 describes the two groups;
+#' @param data A data frame in tidy format. Column 1 describes the two groups;
 #'   column 2 contains the values for each group. A properly formatted data
-#'   frame can be created using \code{link{mkt2}} or \code{link{mkt2d}}.
+#'   frame can be created using \code{link{mkt2}} or \code{link{mkt2d}}. Missing
+#'   values are not allowed.
 #' @param formula A formula with format response variable ∼ predictor variable,
 #'   where ~ (tilde) means "is modeled as a function of".
 #' @param nboot The number of bootstrap samples - default = 200.
@@ -129,7 +131,7 @@ shifthd <- function(data = df, formula = obs ~ gr, nboot = 200){
 shiftdhd <- function(data = df, formula = obs ~ gr, nboot = 200){
   # subset data
   out <- subset_data2(data, formula)
-  df <- na.omit(df) # remove NA
+  #df <- na.omit(df) # remove NA
   x <- out$x
   y <- out$y
   gr_name1 <- out$gr_name1
@@ -174,20 +176,29 @@ shiftdhd <- function(data = df, formula = obs ~ gr, nboot = 200){
 #' \item \code{q = seq(.1, .9, .1)}
 #' \item \code{q = seq(.05, .95, .05)}
 #' }
-#' @param x,y Vectors; no missing values are allowed
+#' @param data A data frame in tidy format. Column 1 describes the two groups;
+#'   column 2 contains the values for each group. A properly formatted data
+#'   frame can be created using \code{link{mkt2}} or \code{link{mkt2d}}. Missing
+#'   values are not allowed.
+#' @param formula A formula with format response variable ∼ predictor variable,
+#'   where ~ (tilde) means "is modeled as a function of".
 #' @param q Quantiles to estimate - default = deciles 0.1:0.1:.9.
-#' Other suggestions
 #' @param nboot Number of bootstrap samples - default = 2000
 #' @param alpha Expected long-run type I error rate - default = 0.05
+#' @param adj_ci Adjust confidence intervals & p values for multiple comparisons - default = TRUE
 #' @return A data frame with one row per decile.
 #' The columns are: \itemize{
-#'   \item Column 1 = quantiles for group 1
-#'   \item Column 2 = quantiles for group 2
-#'   \item Column 3 = differences (column 1 - column 2)
-#'   \item Column 4 = lower bounds of the confidence intervals
-#'   \item Column 5 = upper bounds of the confidence intervals
-#'   \item Column 6 = p-values based on Hochberg's method
-#'   \item Column 7 = critical p-values based on Hochberg's method
+#'   \item Column 1 = quantiles
+#'   \item Column 2 = number of observations in group 1
+#'   \item Column 3 = number of observations in group 2
+#'   \item Column 4 = quantiles for group 1
+#'   \item Column 5 = quantiles for group 2
+#'   \item Column 6 = quantile differences (column 4 - column 5)
+#'   \item Column 7 = lower bounds of the confidence intervals
+#'   \item Column 8 = upper bounds of the confidence intervals
+#'   \item Column 9 = critical p-values based on Hochberg's method
+#'   \item Column 10 = p-values based on Hochberg's method
+#'   \item Column 11 = significance 0/1
 #'   }
 #' @section Note:
 #' Adaptation of Rand Wilcox's `qcomhd` & `pb2gen` R functions
@@ -208,177 +219,162 @@ shifthd_pbci <- function(data = df,
                          adj_ci = TRUE){
   # subset data
   out <- subset_data2(data, formula)
-  df <- na.omit(df) # remove NA
+  #df <- na.omit(df) # remove NA
   x <- out$x
   y <- out$y
   gr_name1 <- out$gr_name1
   gr_name2 <- out$gr_name2
   # declare matrix of results
-  m = matrix(0, nrow = length(q), ncol = 9)
+  output = matrix(0, nrow = length(q), ncol = 10)
+  # confidence interval's boundaries
+  low <- round((alpha / 2) * nboot) + 1
+  up <- nboot - low
   # loop through quantiles
   for(i in 1:length(q)){
-    output[i,1]=q[i]
-    output[i,2]=length(elimna(x))
-    output[i,3]=length(elimna(y))
-    output[i,4]=hd(x,q=q[i])
-    output[i,5]=hd(y,q=q[i])
-    output[i,6]=output[i,4]-output[i,5]
-    temp=pb2gen(x,y,nboot=nboot,est=hd,q=q[i],SEED=FALSE,alpha=alpha,pr=FALSE)
-    output[i,7]=temp$ci[1]
-    output[i,8]=temp$ci[2]
-    output[i,10]=temp$p.value
+    output[i,1] = q[i]
+    output[i,2] = length(elimna(x))
+    output[i,3] = length(elimna(y))
+    output[i,4] = hd(x, q = q[i])
+    output[i,5] = hd(y, q = q[i])
+    output[i,6] = output[i,4]-output[i,5]
+    #  bootstrap
+    datax <- matrix(sample(x, size = length(x) * nboot, replace = TRUE), nrow = nboot)
+    datay <- matrix(sample(y, size = length(y) * nboot, replace = TRUE), nrow = nboot)
+    bvecx <- apply(datax, 1, hd, q = q[i])
+    bvecy <- apply(datay, 1, hd, q = q[i])
+    bvec <- sort(bvecx - bvecy)
+    temp <- sum(bvec < 0) / nboot + sum(bvec == 0) / (2 * nboot)
+    output[i,7] = bvec[low] # ci_lower
+    output[i,8] = bvec[up] # ci_upper
+    output[i,10] = 2*(min(temp,1-temp)) # p-value
   }
-  temp=order(output[,10],decreasing=TRUE)
-  zvec=alpha/c(1:length(q))
-  output[temp,9]=zvec
-  if(ADJ.CI){
+  temp = order(output[,10], decreasing=TRUE)
+  zvec = alpha / c(1:length(q))
+  output[temp,9] = zvec # p_crit
+  if(adj_ci){
     for(i in 1:length(q)){
-      temp=pb2gen(x,y,nboot=nboot,est=hd,q=q[i],SEED=FALSE,alpha=output[i,9],pr=FALSE)
-      output[i,7]=temp$ci[1]
-      output[i,8]=temp$ci[2]
-      output[i,10]=temp$p.value
+      alpha = output[i,9]
+      # confidence interval's boundaries
+      low <- round((alpha / 2) * nboot) + 1
+      up <- nboot - low
+      #  bootstrap
+      datax <- matrix(sample(x, size = length(x) * nboot, replace = TRUE), nrow = nboot)
+      datay <- matrix(sample(y, size = length(y) * nboot, replace = TRUE), nrow = nboot)
+      bvecx <- apply(datax, 1, hd, q = q[i])
+      bvecy <- apply(datay, 1, hd, q = q[i])
+      bvec <- sort(bvecx - bvecy)
+      temp <- sum(bvec < 0) / nboot + sum(bvec == 0) / (2 * nboot)
+      output[i,7] = bvec[low] # ci_lower
+      output[i,8] = bvec[up] # ci_upper
+      output[i,10] = 2*(min(temp,1-temp)) # p-value
     }
   }
   # make data frame
   out <- data.frame(m)
-  names(out) <- c('q', gr_name1, gr_name2, 'difference',
+  names(out) <- c('q', 'n1', 'n2', gr_name1, gr_name2, 'difference',
                   'ci_lower', 'ci_upper', 'p_crit', 'p-value')
   # add sig column
   dplyr::mutate(out, sig = p-value <= p_crit)
   out
 }
 
-pb2gen<-function(x,y,alpha=.05,nboot=2000,est=onestep,SEED=TRUE,pr=FALSE,...){
-  #
-  #   Compute a bootstrap confidence interval for the
-  #   the difference between any two parameters corresponding to
-  #   independent groups.
-  #   By default, M-estimators are compared.
-  #   Setting est=mean, for example, will result in a percentile
-  #   bootstrap confidence interval for the difference between means.
-  #   Setting est=onestep will compare M-estimators of location.
-  #   The default number of bootstrap samples is nboot=2000
-  #
-  x<-x[!is.na(x)] # Remove any missing values in x
-  y<-y[!is.na(y)] # Remove any missing values in y
-  if(SEED)set.seed(2) # set seed of random number generator so that
-  #             results can be duplicated.
-  datax<-matrix(sample(x,size=length(x)*nboot,replace=TRUE),nrow=nboot)
-  datay<-matrix(sample(y,size=length(y)*nboot,replace=TRUE),nrow=nboot)
-  bvecx<-apply(datax,1,est,...)
-  bvecy<-apply(datay,1,est,...)
-  bvec<-sort(bvecx-bvecy)
-  low<-round((alpha/2)*nboot)+1
-  up<-nboot-low
-  temp<-sum(bvec<0)/nboot+sum(bvec==0)/(2*nboot)
-  sig.level<-2*(min(temp,1-temp))
-  se<-var(bvec)
-  list(est.1=est(x,...),est.2=est(y,...),est.dif=est(x,...)-est(y,...),ci=c(bvec[low],bvec[up]),p.value=sig.level,sq.se=se,n1=length(x),n2=length(y))
-}
-
-
-#' Shift function for two depend groups (pbci methods)
+#' Shift function for two depend groups (pbci method)
 #'
-#' This version of the shift function
-#' Computes a shift function for two dependent groups by comparing
-#' the quantiles of the marginal distributions using the
+#' Compute a shift function for two dependent groups using the
 #' Harrell-Davis quantile estimator in conjunction with a percentile
 #' bootstrap approach.
-#'
-#' INPUTS:
-#' - x & y are vectors of the same length without missing values
-#' - q = quantiles to estimate - default = 0.1:0.1:.9 (deciles)
-#'- nboot = number of bootstrap samples - default = 2000
-#' - alpha = expected long-run type I error rate - default = 0.05
-#' - plotit = 1 to get a figure; 0 otherwise by default
-#'
-#' OUTPUTS:
-#'  - xd & yd = vectors of quantiles
-#' - delta = vector of differences between quantiles (x-y)
-#' - deltaCI = matrix quantiles x low/high bounds of the confidence
-#' intervals of the quantile differences
-#'
-#' Unlike shiftdhd:
-#' - the confidence intervals are not corrected for multiple comparisons
-#' (the R version provides corrected critical p values - here i've decided
-#' not to provide p values at all - the goal is to understand how
-#' distributions differ, not to make binary decisions)
-#' - the confidence intervals are calculated using a percentile bootstrap of
-#'   the quantiles, instead of a percentile bootstrap of the standard error of the quantiles
-#' - the quantiles to compare can be specified and are not limited to the
-#'   deciles
-#' - Tied values are allowed
-#'
-#' Unlike Rand Wilcox's Dqcomhd R function, no p value is returned.
-#'  Extensive experience suggests humans cannot be trusted with p values.
-#'
+#' Unlike \code{\link{shiftdhd}}: \itemize{
+#' \item The confidence intervals are calculated using a percentile bootstrap of
+#' the quantiles, instead of a percentile bootstrap of the standard error of
+#' the difference of the quantiles.
+#' \item The quantiles can be specified and are not limited to the deciles.
+#' \item Tied values are allowed.
+#' }
+#' The confidence intervals are not corrected for multiple comparisons, the p
+#' values are.
+#' Examples of quantile sequences, from sparse to dense: \itemize{
+#' \item \code{q = c(.25,.5,.75)}
+#' \item \code{q = c(.1,.25,.5,.75,.9)}
+#' \item \code{q = seq(.1, .9, .1)}
+#' \item \code{q = seq(.05, .95, .05)}
+#' }
+#' @param data A data frame in tidy format. Column 1 describes the two groups;
+#'   column 2 contains the values for each group. A properly formatted data
+#'   frame can be created using \code{link{mkt2}} or \code{link{mkt2d}}. Missing
+#'   values are not allowed.
+#' @param formula A formula with format response variable ∼ predictor variable,
+#'   where ~ (tilde) means "is modeled as a function of".
+#' @param q Quantiles to estimate - default = deciles 0.1:0.1:.9.
+#' @param nboot Number of bootstrap samples - default = 1000
+#' @param alpha Expected long-run type I error rate - default = 0.05
+#' @return A data frame with one row per decile.
+#' The columns are: \itemize{
+#'   \item Column 1 = quantiles
+#'   \item Column 2 = number of observations
+#'   \item Column 3 = quantiles for group 1
+#'   \item Column 4 = quantiles for group 2
+#'   \item Column 5 = quantile differences (column 3 - column 4)
+#'   \item Column 6 = lower bounds of the confidence intervals
+#'   \item Column 7 = upper bounds of the confidence intervals
+#'   \item Column 8 = critical p-values based on Hochberg's method
+#'   \item Column 9 = p-values
+#'   \item Column 10 = significance 0/1
+#'   }
 #' @section Note:
-#' Adaptation of Rand Wilcox's Dqcomhd, bootdpci & rmmcppb R functions,
-#' http://dornsife.usc.edu/labs/rwilcox/software/
-#' From Rallfun-v32.txt - see \url{https://github.com/nicebread/WRS/}
-#' @seealso HD, SHIFTDHD, SHIFTHD_PBCI
+#' Adaptation of Rand Wilcox's `Dqcomhd`, `bootdpci` & `rmmcppb` R functions
+#' (\url{http://dornsife.usc.edu/labs/rwilcox/software/}).
+#' From Rallfun-v32.txt - see \url{https://github.com/nicebread/WRS/}.
+#'
 #' @references
 #' Wilcox, R.R. & Erceg-Hurn, D.M. (2012)
 #' Comparing two dependent groups via quantiles.
 #' J Appl Stat, 39, 2655-2664.
-shiftdhd_pbci <- function(){
-
+#' @seealso \code{hd}, \code{shiftdhd} for the pbse method for dependent
+#'   groups, \code{shifthd_pbci} for dependent groups
+shiftdhd_pbci <- function(data = df,
+                          formula = obs ~ gr,
+                          q = seq(.1, .9, .1),
+                          nboot = 1000,
+                          alpha = 0.05){
+  # subset data
+  out <- subset_data2(data, formula)
+  #df <- na.omit(df) # remove NA
+  x <- out$x
+  y <- out$y
+  gr_name1 <- out$gr_name1
+  gr_name2 <- out$gr_name2
+  # declare matrix of results
+  output = matrix(0, nrow = length(q), ncol = 9)
+  # confidence interval's boundaries
+  low <- round((alpha / 2) * nboot) + 1
+  up <- nboot - low
+  n <- length(x)
+  # loop through quantiles
+  for(i in 1:length(q)){
+    output[i,1] = q[i]
+    output[i,2] = n
+    output[i,3] = hd(x, q = q[i])
+    output[i,4] = hd(y, q = q[i])
+    output[i,5] = output[i,3]-output[i,4]
+    #  bootstrap
+    bootsample <- matrix(sample(n, size = n * nboot, replace = TRUE), nrow = nboot)
+    xmat <- matrix(x[bootsample], nrow = nboot, ncol = n)
+    ymat <- matrix(y[bootsample], nrow = nboot, ncol = n)
+    bvec <- apply(xmat, 1, hd, q = q[i]) - apply(ymat, 1, hd, q = q[i])
+    bvec <- sort(bvec[,1] - bvec[,2])
+    temp <- sum(bvec < 0) / nboot + sum(bvec == 0) / (2 * nboot)
+    output[i,6] = bvec[low] # ci_lower
+    output[i,7] = bvec[up] # ci_upper
+    output[i,9] = 2*(min(temp,1-temp)) # p-value
+  }
+  temp = order(output[,9], decreasing=TRUE)
+  zvec = alpha / c(1:length(q))
+  output[temp,8] = zvec # p_crit
+  # make data frame
+  out <- data.frame(m)
+  names(out) <- c('q', 'n', gr_name1, gr_name2, 'difference',
+    'ci_lower', 'ci_upper', 'p_crit', 'p-value')
+  # add sig column
+  dplyr::mutate(out, sig = p-value <= p_crit)
+  out
 }
-
-# Dqcomhd<-function(x,y,q=c(1:9)/10,nboot=1000,plotit=TRUE,SEED=TRUE,xlab="Group 1",ylab="Est.1-Est.2",na.rm=TRUE,alpha=.05){
-# #
-# # Compare the quantiles of the marginal distributions associated with  two dependent groups
-# # via hd estimator. Tied values are allowed.
-# # When comparing lower or upper quartiles, both power and the probability of Type I error
-# # compare well to other methods have been derived.
-# #
-# #  x: data for group 1
-# #  y: data for group 2
-# #  q: the quantiles to be compared
-# #  nboot: Number of bootstrap samples
-# #
-# #
-# if(SEED)set.seed(2)
-#
-# xy=elimna(cbind(x,y))
-# x=xy[,1]
-# y=xy[,2]
-#
-# pv=NULL
-# output=matrix(NA,nrow=length(q),ncol=10)
-# dimnames(output)<-list(NULL,c("q","n1","n2","est.1","est.2","est.1_minus_est.2","ci.low","ci.up","p_crit","p-value"))
-# for(i in 1:length(q)){
-# output[i,1]=q[i]
-# output[i,2]=length(elimna(x))
-# output[i,3]=length(elimna(y))
-# output[i,4]=hd(x,q=q[i])
-# output[i,5]=hd(y,q=q[i])
-# output[i,6]=output[i,4]-output[i,5]
-#
-# temp=bootdpci(x,y,est=hd,q=q[i],dif=FALSE,plotit=FALSE,pr=FALSE,nboot=nboot,alpha=alpha,SEED=FALSE)
-# output[i,7]=temp$output[1,5]
-# output[i,8]=temp$output[1,6]
-# output[i,10]=temp$output[1,3]
-#
-# }
-# }
-# if(plotit){
-# xax=rep(output[,4],3)
-# yax=c(output[,6],output[,7],output[,8])
-# plot(xax,yax,xlab=xlab,ylab=ylab,type="n")
-# points(output[,4],output[,6],pch="*")
-# lines(output[,4],output[,6])
-# points(output[,4],output[,7],pch="+")
-# points(output[,4],output[,8],pch="+")
-# }
-# temp=order(output[,10],decreasing=TRUE)
-# zvec=alpha/c(1:length(q))
-# output[temp,9]=zvec
-# output <- data.frame(output)
-# output$signif=rep("YES",nrow(output))
-# for(i in 1:nrow(output)){
-# if(output[temp[i],10]>output[temp[i],9])output$signif[temp[i]]="NO"
-# if(output[temp[i],10]<=output[temp[i],9])break
-# }
-# output
-# }
